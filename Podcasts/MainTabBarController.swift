d46742b71a1f07f7f4de867f5c075a4b6f2af673
Nihaal Manesia <nihaal419@gmail.com>
2019-07-29 21:44:42 -0500

    Basic Tab Bar Layout update

---
+++ Podcasts/MainTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarController.swift
+//  Podcasts
+//
+//  Created by Nihaal Manesia on 7/16/19.
+//  Copyright © 2019 Nihaal Manesia. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        setupViewControllers()
+    }
+    
+    fileprivate func setupViewControllers() {
+        viewControllers = [
+            generateNavigationControllers(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorite")),
+            generateNavigationControllers(with: PodcastSearchController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationControllers(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "download"))
+        ]
+    }
+    
+    //MARK:- Helper Functions
+    
+    fileprivate func generateNavigationControllers(with rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+}
---
+++ Podcasts/PodcastSearchController.swift
@@ -0,0 +1,15 @@
+//
+//  PodcastSearchController.swift
+//  Podcasts
+//
+//  Created by Nihaal Manesia on 7/16/19.
+//  Copyright © 2019 Nihaal Manesia. All rights reserved.
+//
+
+import UIKit
+
+class PodcastSearchController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
